4fc20b9b59f6c4492caf0279e6558c0b9107d477
farizkyy <147523622+farizkyy@users.noreply.github.com>
2024-10-12 16:32:22 +0700

    Update burung.java

--- java/burung.java
+++ java/burung.java
@@ -1,12 +1,11 @@
-class burung extends Hewan {
+class Burung extends Hewan {
     
-    public burung(String nama, int umur) {
+    public Burung(String nama, int umur) {
         super(nama, umur);
     }
 
-    // Overriding metode suara
     @Override
     public void suara() {
-        System.out.println(getNama() + " berkata: berkicau!");
-    }
+        System.out.println(getNama() + " berkata: Tweet!");
+    }
 }
