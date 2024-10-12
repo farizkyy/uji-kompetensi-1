d971a70c59c1ebd4a839d21439d00fca5033a90a
farizkyy <147523622+farizkyy@users.noreply.github.com>
2024-10-12 16:30:49 +0700

    Update main.java

--- java/main.java
+++ java/main.java
@@ -1,10 +1,9 @@
 public class main {
     
     public static void main(String[] args) {
-
-        kucing kucing = new kucing("Kitty", 2);
-        anjing anjing = new anjing("Buddy", 3);
-        burung burung = new burung("Tweety", 1);
+        Kucing kucing = new Kucing("Kitty", 2);
+        Anjing anjing = new Anjing("Buddy", 3);
+        Burung burung = new Burung("Tweety", 1);
 
         kucing.suara();
         anjing.suara();
@@ -13,15 +12,12 @@ public class main {
         kucing.setUmur(3);
         System.out.println(kucing.getNama() + " sekarang berumur " + kucing.getUmur() + " tahun.");
         
-        anjing anjing2 = new anjing("Max", 4);
-        
+        Anjing anjing2 = new Anjing("Max", 4);
         System.out.println(anjing2.getNama() + " berumur " + anjing2.getUmur() + " tahun.");
         
-
         anjing2.setNama("Rocky");
         System.out.println("Nama baru anjing adalah: " + anjing2.getNama());
         
-
         Hewan[] hewanArray = {kucing, anjing, burung};
         
         for (Hewan hewan : hewanArray) {
